19f29aeb2ae5b6507b3fef7eb2f563a9af720564
Leandro Serra <systemserra@hotmail.com>
2024-11-26 11:26:28 -0300

    [API] Ajustes de pacotes desatualizados e com vulnerabilidades

--- src/EncontroDeLutadores.Aplicacao/DTOs/Identity/UsuarioCriacaoDTO.cs
+++ src/EncontroDeLutadores.Aplicacao/DTOs/Identity/UsuarioCriacaoDTO.cs
@@ -8,7 +8,7 @@ namespace EncontroDeLutadores.Aplicacao.DTOs.Identity
 {
     public class UsuarioCriacaoDTO
     {
-        public string Nome { get;  set; }
+        public string Nome { get; set; }
         public string Sobrenome { get;  set; }
         public string Email { get; set; }
         public string Password { get; set; }
